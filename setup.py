7542fe7b7c5bcc90a76f6c6e1dcf725bfcd0a293
bb515 <bb515@cam.ac.uk>
2022-12-01 07:38:27 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -17,21 +17,18 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="sgm",
-    #version="0.1.0",
+    version="0.1.0",
     #description="",
     long_description=README,
     long_description_content_type="text/markdown",
     #url="",
-    #author="Benjamin Boys",
+    #author="Jakiw Pidstrigach and Benjamin Boys",
     #license="MIT",
     packages=find_packages(exclude=['*.test']),
     install_requires=[
-        'backends==1.4.31',
-        'mlkernels==0.3.6',
         'numpy==1.23.4',
         'scipy==1.9.2',
         'tqdm==4.64.1',
-        'h5py==2.10.0',
         'matplotlib==3.6.1',
         'optax==0.1.3',
         'jax==0.3.23',
