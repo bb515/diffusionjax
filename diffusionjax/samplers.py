61da4952ff9ac3c5938921047f093012c7fa4386
bb515 <bb515@cam.ac.uk>
2023-02-24 16:21:13 +0000

    removed redundant code

--- diffusionjax/samplers.py
+++ diffusionjax/samplers.py
@@ -6,7 +6,7 @@ from diffusionjax.utils import batch_mul
 
 
 class EulerMaruyama():
-    """Euler Maruyama numerical solver of an SDE.Functions are designed for a mini-batch of inputs."""
+    """Euler Maruyama numerical solver of an SDE. Functions are designed for a mini-batch of inputs."""
 
     def __init__(self, sde, score_fn):
         """Constructs an Euler Maruyama sampler.
@@ -39,24 +39,6 @@ class EulerMaruyama():
             x = x_mean + batch_mul(G, z)
             return x, x_mean
         return update
-
-    def get_sampler_enumerate(self):
-        ts = self.sde.train_ts
-        update = self.get_update()
-        def sampler(rng, n_samples, shape):
-            rng, step_rng = random.split(rng)
-            n_samples_shape = (n_samples,) + shape
-            print(n_samples_shape)
-            x = random.normal(step_rng, n_samples_shape)
-            def f(carry, t):
-                rng, x, x_mean, i = carry
-                vec_t = jnp.ones((n_samples, 1)) * (1 - t)
-                rng, step_rng = random.split(rng)
-                x, x_mean = update(step_rng, x, vec_t)
-                i += 1
-                return (rng, x, x_mean, i), ()
-            (_, x, _), _ = scan(f, (rng, x, x, 0), ts)
-            return x
 
     def get_sampler(self, stack_samples=False):
         ts = self.sde.train_ts
