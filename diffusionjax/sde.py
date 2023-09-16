removed horrible try except clause. fix example.py bug to do with scaling the data. remove dependence on prod from python3.8

diffusionjax/sde.py
@@ -162,11 +162,7 @@ class VP(SDE):
             t: JAX float of the time
         """
         m = self.mean_coeff(t)
-        # TODO: remove this try except clause
-        try:
-            mean = batch_mul(m, x)
-        except:
-            mean = m * x
+        mean = batch_mul(m, x)
         std = jnp.sqrt(self.variance(t))
         return mean, std
 

diffusionjax/utils.py
@@ -161,7 +161,7 @@ def retrain_nn(
 
 def get_score(sde, model, params, score_scaling):
     if score_scaling is True:
-        return lambda x, t: -batch_mul(model.apply(params, x, t), 1. / sde.marginal_prob(x, t)[1])
+        return lambda x, t: -batch_mul(model.apply(params, x, t), 1. / jnp.sqrt(sde.variance(t)))
     else:
         return lambda x, t: -model.apply(params, x, t)
 