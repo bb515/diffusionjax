8a49e256a94b5c733b1bfbe5e2eef70302662533
bb515 <bb515@cam.ac.uk>
2024-07-01 17:12:28 +0100

    add UNet

--- diffusionjax/models/networks_edmv2.py
+++ diffusionjax/models/networks_edmv2.py
@@ -7,6 +7,13 @@ import jax
 import jax.numpy as jnp
 import flax.linen as nn
 
+from typing import Any
+
+
+def jax_unstack(x, axis=0):
+  """https://github.com/google/jax/discussions/11028"""
+  return [jax.lax.index_in_dim(x, i, axis, keepdims=False) for i in range(x.shape[axis])]
+
 
 def pixel_normalize(x, channel_axis, eps=1e-4):
   """
@@ -53,8 +60,8 @@ def resample(x, f=[1, 1], mode="keep"):
       x,
       jnp.tile(f, (c, 1, 1, 1)),
       window_strides=(2, 2),
-      feature_group_count=3,
-      padding="SAME",  # not sure
+      feature_group_count=c,
+      padding="SAME",
     )
   assert mode == "up"
 
@@ -65,7 +72,7 @@ def resample(x, f=[1, 1], mode="keep"):
     dimension_numbers=("NCHW", "OIHW", "NCHW"),
     window_strides=(1, 1),
     lhs_dilation=(2, 2),
-    feature_group_count=3,
+    feature_group_count=c,
     padding=((pad, pad), (pad, pad)),
   )
 
@@ -87,7 +94,7 @@ def mp_cat(a, b, dim=1, t=0.5):
   C = jnp.sqrt((Na + Nb) / ((1 - t) ** 2 + t**2))
   wa = C / jnp.sqrt(Na) * (1 - t)
   wb = C / jnp.sqrt(Nb) * t
-  return jax.lax.concatenate([wa * a, wb * b], dim)
+  return jax.lax.concatenate([wa * a, wb * b], dimension=dim)
 
 
 class MPFourier(nn.Module):
@@ -135,7 +142,6 @@ class MPConv(nn.Module):
       w = jax.lax.stop_gradient(w)
       w = weight_normalize(w)  # forced weight normalization
 
-    print(w.shape)
     w = weight_normalize(w)  # traditional weight normalization
     w = w * (gain / jnp.sqrt(w[0].size))  # magnitude-preserving scaling
     if w.ndim == 2:
@@ -163,11 +169,10 @@ class Block(nn.Module):
   resample_filter: tuple = (1, 1)  # Resampling filter.
   attention: bool = False  # Include self-attention?
   channels_per_head: int = 64  # Number of channels per attention head.
-  dropout: float or bool = 0.0  # Dropout probability.
+  dropout: float = 0.0  # Dropout probability.
   res_balance: float = 0.3  # Balance between main branch (0) and residual branch (1).
   attn_balance: float = 0.3  # Balance between main branch (0) and self-attention (1).
   clip_act: int = 256  # Clip output activations. None = do not clip.
-  num_heads: int = out_channels // channels_per_head if attention else 0
   training: bool = True
 
   @nn.compact
@@ -210,14 +215,16 @@ class Block(nn.Module):
     x = mp_sum(x, y, t=self.res_balance)
 
     # Self-attention
-    # TODO: test if flax.linen.attention can be used instead here
-    if self.num_heads != 0:
+    # TODO: test if flax.linen.SelfAttention can be used instead here
+    num_heads = self.out_channels // self.channels_per_head if self.attention else 0
+    if num_heads != 0:
       y = MPConv(
         self.out_channels, self.out_channels * 3, kernel_shape=[1, 1], name="attn_qkv"
       )(x)
-      y = y.reshape(y.shape[0], self.num_heads, -1, 3, y.shape[2] * y.shape[3])
-      q, k, v = pixel_normalize(y, channel_axis=2)  # pixel normalization and split
-      w = jnp.softmax(jnp.einsum("nhcq,nhck->nhqk", q, k / jnp.sqrt(q.shape[2])))
+      y = y.reshape(y.shape[0], num_heads, -1, 3, y.shape[2] * y.shape[3])
+      q, k, v = jax_unstack(pixel_normalize(y, channel_axis=2), axis=3)  # pixel normalization and split
+      # NOTE: quadratic cost in last dimension
+      w = nn.softmax(jnp.einsum("nhcq,nhck->nhqk", q, k / jnp.sqrt(q.shape[2])), axis=3)
       y = jnp.einsum("nhqk,nhck->nhcq", w, v)
       y = MPConv(
         self.out_channels, self.out_channels, kernel_shape=[1, 1], name="attn_proj"
@@ -228,3 +235,166 @@ class Block(nn.Module):
     if self.clip_act is not None:
       x = jnp.clip(x, -self.clip_act, self.clip_act)
     return x
+
+
+class UNet(nn.Module):
+  """EDM2 U-Net model (Figure 21)."""
+
+  img_resolution: int  # Image resolution.
+  img_channels: int  # Image channels.
+  label_dim: int  # Class label dimensionality. 0 = unconditional.
+  model_channels: int = 192  # Base multiplier for the number of channels.
+  channel_mult: tuple = (
+    1,
+    2,
+    3,
+    4,
+  )  # Per-resolution multipliers for the number of channels.
+  channel_mult_noise: Any = None  # Multiplier for noise embedding dimensionality. None = select based on channel_mult.
+  channel_mult_emb: Any = None  # Multiplier for final embedding dimensionality. None = select based on channel_mult.
+  num_blocks: int = 3  # Number of residual blocks per resolution.
+  attn_resolutions: tuple = (16, 8)  # List of resolutions with self-attention.
+  label_balance: float = (
+    0.5  # Balance between noise embedding (0) and class embedding (1).
+  )
+  concat_balance: float = 0.5  # Balance between skip connections (0) and main path (1).
+
+  # **block_kwargs - arguments for Block
+  resample_filter: tuple = (1, 1)  # Resampling filter
+  channels_per_head: int = 64  # Number of channels per attention head
+  dropout: float = 0.0  # Dropout probability
+  res_balance: float = 0.3  # Balance between main branch (0) and residual branch (1)
+  attn_balance: float = 0.3  # Balance between main branch (0) and self-attention (1)
+  clip_act: int = 256  # Clip output activations. None = do not clip
+  out_gain: Any = None
+  block_kwargs = {
+    'resample_filter': resample_filter,
+    'channels_per_head': channels_per_head,
+    'dropout': dropout,
+    'res_balance': res_balance,
+    'attn_balance': attn_balance,
+    'clip_act': clip_act,
+  }
+
+  @nn.compact
+  def __call__(self, x, noise_labels, class_labels):
+    cblock = [self.model_channels * x for x in self.channel_mult]
+    cnoise = (
+      self.model_channels * self.channel_mult_noise
+      if self.channel_mult_noise is not None
+      else cblock[0]
+    )
+    cemb = (
+      self.model_channels * self.channel_mult_emb
+      if self.channel_mult_emb is not None
+      else max(cblock)
+    )
+
+    if self.out_gain is None:
+      out_gain = self.param("out_gain", jax.nn.initializers.zeros, (1,))
+    else:
+      out_gain = self.out_gain
+
+
+    # Encoder
+    enc = {}
+    cout = self.img_channels + 1
+    for level, channels in enumerate(cblock):
+      res = self.img_resolution >> level
+      if level == 0:
+        cin = cout
+        cout = channels
+        enc[f"{res}x{res}_conv"] = MPConv(
+          cin, cout, kernel_shape=[3, 3], name=f"enc_{res}x{res}_conv"
+        )
+      else:
+        enc[f"{res}x{res}_down"] = Block(
+          cout,
+          cout,
+          cemb,
+          flavor="enc",
+          resample_mode="down",
+          name=f"enc_{res}x{res}_down",
+          **self.block_kwargs,
+        )
+      for idx in range(self.num_blocks):
+        cin = cout
+        cout = channels
+        enc[f"{res}x{res}_block{idx}"] = Block(
+          cin,
+          cout,
+          cemb,
+          flavor="enc",
+          attention=(res in self.attn_resolutions),
+          name=f"enc_{res}x{res}_block{idx}",
+          **self.block_kwargs,
+        )
+
+    # Decoder
+    dec = {}
+    skips = [block.out_channels for block in enc.values()]
+    for level, channels in reversed(list(enumerate(cblock))):
+      res = self.img_resolution >> level
+      if level == len(cblock) - 1:
+        dec[f"{res}x{res}_in0"] = Block(
+          cout,
+          cout,
+          cemb,
+          flavor="dec",
+          attention=True,
+          name=f"dec_{res}x{res}_in0",
+          **self.block_kwargs,
+        )
+        dec[f"{res}x{res}_in1"] = Block(
+          cout, cout, cemb, flavor="dec", name=f"dec_{res}x{res}_in1", **self.block_kwargs
+        )
+      else:
+        dec[f"{res}x{res}_up"] = Block(
+          cout,
+          cout,
+          cemb,
+          flavor="dec",
+          resample_mode="up",
+          name=f"dec_{res}x{res}_up",
+          **self.block_kwargs,
+        )
+      for idx in range(self.num_blocks + 1):
+        cin = cout + skips.pop()
+        cout = channels
+        dec[f"{res}x{res}_block{idx}"] = Block(
+          cin,
+          cout,
+          cemb,
+          flavor="dec",
+          attention=(res in self.attn_resolutions),
+          name=f"dec_{res}x{res}_block{idx}",
+          **self.block_kwargs,
+        )
+
+    # Embedding
+    emb = MPConv(cnoise, cemb, kernel_shape=[], name="emb_noise")(MPFourier(cnoise, name="emb_fourier")(noise_labels))
+    if self.label_dim != 0:
+      emb = mp_sum(
+        emb,
+        MPConv(self.label_dim, cemb, kernel_shape=[], name="emb_label")(class_labels * np.sqrt(class_labels.shape[1])),
+        t=self.label_balance,
+      )
+    emb = mp_silu(emb)
+
+    # Encoder
+    x = jax.lax.concatenate([x, jnp.ones_like(x[:, :1])], dimension=1)
+    skips = []
+    for name, block in enc.items():
+      x = block(x) if "conv" in name else block(x, emb)
+      skips.append(x)
+
+    # Decoder
+    for name, block in dec.items():
+      if "block" in name:
+        x = mp_cat(x, skips.pop(), t=self.concat_balance)
+      x = block(x, emb)
+    x = MPConv(cout, self.img_channels, kernel_shape=[3, 3], name="out_conv")(
+      x, gain=out_gain)
+    return x
+
+
